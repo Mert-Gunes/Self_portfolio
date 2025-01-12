37797df7e3bc3c53ce19ec75679e7f48f5ec48bb
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2025-01-13 02:31:08 +0300

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,24 +1,12 @@
 export const skillsData = [
-  'HTML',
-  'CSS',
-  'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
-  'Git',
-  'AWS',
-  'Bootstrap',
-  'Docker',
-  'Go',
-  'Figma',
-  'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
+  'Python',
+  'Canva',
+  'Dart',
+  'Flutter',
+  'Ruby',
+  'Numpy',
+  'C',
+  'Unity',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
